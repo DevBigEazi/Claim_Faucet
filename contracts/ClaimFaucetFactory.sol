d797b6770eeff8382bf9613d564055b4b12242d7
devbigeazi <adesholatajudeen1@gmail.com>
2024-10-17 13:07:54 +0100

    Added a contract factory

---
+++ contracts/ClaimFaucetFactory.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { ClaimFaucet } from"./ClaimFaucet.sol";
+
+contract ClaimFaucetFactory {
+
+    struct DeployedContractInfo {
+        address deployer;
+        address deployedContract;
+    }
+
+    mapping (address => DeployedContractInfo[])allUserDeployedContracts;
+
+    DeployedContractInfo[] allContracts;
+
+    function deployClaimFaucet(string memory _name, string memory _symbol) external returns (address contractAddress_) {
+        require(msg.sender != address(0), "Zero Address not allowed");
+
+        address _address = address(new ClaimFaucet(_name, _symbol));
+        contractAddress_ = _address;
+
+        DeployedContractInfo memory _deployedContract;
+        _deployedContract.deployer = msg.sender;
+        _deployedContract.deployedContract = _address;
+
+        allUserDeployedContracts[msg.sender].push(_deployedContract);
+
+        allContracts.push(_deployedContract);
+    }
+
+    function getAllContractDeployed() external view returns (DeployedContractInfo[] memory){
+        return allContracts;
+    }
+
+    function getUserDeployedContracts() external view returns (DeployedContractInfo[] memory) {
+        return allUserDeployedContracts[msg.sender];
+    }
+
+    function getUserDeployedContractByIndex(uint8 _index) external view returns (address deployer_, address deployedContract_) {
+        require(_index < allUserDeployedContracts[msg.sender].length, "Out of bound");
+
+        DeployedContractInfo memory _deployedContract = allUserDeployedContracts[msg.sender][_index];
+
+        deployer_ = _deployedContract.deployer;
+        deployedContract_ = _deployedContract.deployedContract;   
+    }
+
+    function getLengthOfDeployedContracts() external view returns (uint256) {
+        uint256 lens = allContracts.length;
+
+        return lens;
+    }
+
+}
